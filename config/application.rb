189a572c78bba6d0468208476a27b9ef05f6c514
valleymouth1216 <104252479+valleymouth1216@users.noreply.github.com>
2022-08-24 14:10:02 +0000

    20220824-1

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module EveryonePlantDiary
       config.i18n.default_locale = :ja
   config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
   config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag } #レイアウト崩れ
+  config.beginning_of_week = :sunday
   end
 end
