20eb9b16b0606a2102c5b1b28e73dc80a0d430e4
valleymouth1216 <104252479+valleymouth1216@users.noreply.github.com>
2022-08-28 14:56:48 +0000

    20220828-1

--- app/controllers/public/calendar_diaries_controller.rb
+++ app/controllers/public/calendar_diaries_controller.rb
@@ -7,14 +7,19 @@ class Public::CalendarDiariesController < ApplicationController
   end
 
   def filter_by_date
-  #byebug
+
+  @date = params[:date]
   date_arr = params[:date].split("-")
+  @date_year = date_arr[0]
+  @date_month = date_arr[1]
+  @date_date = date_arr[2]
   date =  Date.new(date_arr[0].to_i,date_arr[1].to_i, date_arr[2].to_i)
    #@diaries=Diary.where(start_time: Time.zone.parse(start_time.to_s))
-
+  #byebug
    @diaries = Diary.select do |d|
 
-     d.start_time.to_date == date
+     d.start_time.to_date == date && d.status ==true
+
    end
 
   #arr = []
