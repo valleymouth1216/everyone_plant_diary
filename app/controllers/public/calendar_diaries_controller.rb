1c153bad00da788611c7e22ff3f5b1cbf30553c4
valleymouth1216 <104252479+valleymouth1216@users.noreply.github.com>
2022-09-06 15:06:35 +0000

    20220906

--- app/controllers/public/calendar_diaries_controller.rb
+++ app/controllers/public/calendar_diaries_controller.rb
@@ -10,11 +10,27 @@ class Public::CalendarDiariesController < ApplicationController
     @date_year = date_arr[0]
     @date_month = date_arr[1]
     @date_date = date_arr[2]
+    #binding.pry
     date =  Date.new(date_arr[0].to_i,date_arr[1].to_i, date_arr[2].to_i)
     #byebug
     @diary_dates = DiaryDate.select do |d|
      d.start_time.to_date == date && d.status == true && d.status_admin == true && d.diary_book.status == true && d.diary_book.status_admin == true
    end
+
+    if params[:search] == ''
+    @diary_dates.sort
+    elsif params[:search] == 'newpost'
+      #@diary_dates.sort { |a, b| b[:created_at].to_time.to_i <=> a[:created_at].to_time.to_i }
+      @diary_dates.sort do |a, b|
+        b[:created_at].in_time_zone.to_i <=> a[:created_at].in_time_zone.to_i
+     #binding.pry
+      end
+    elsif params[:search] == 'oldpost'
+      #@diary_dates.sort { |a, b| a[:created_at].to_time.to_i <=> b[:created_at].to_time.to_i }
+      @diary_dates.sort do |a, b|
+        a[:created_at].in_time_zone.to_i <=> b[:created_at].in_time_zone.to_i
+      end
+    end
 
   #arr = []
   #Diary.each do |d|
