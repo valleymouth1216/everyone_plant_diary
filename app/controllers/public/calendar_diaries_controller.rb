e4e659a714e98b9950b4872e5b082f1ee8b4cf1c
valleymouth1216 <104252479+valleymouth1216@users.noreply.github.com>
2022-09-07 13:00:58 +0000

    20220907

--- app/controllers/public/calendar_diaries_controller.rb
+++ app/controllers/public/calendar_diaries_controller.rb
@@ -2,7 +2,8 @@ class Public::CalendarDiariesController < ApplicationController
     before_action :authenticate_customer!,except: [:index]
 
   def index
-    @diary_dates = DiaryDate.where(status_admin: true,status: true).joins(:diary_book).where(status_admin: true,status: true)
+   #@diary_dates = DiaryDate.where(status_admin: true,status: true).joins(:diary_book).where(status_admin: true,status: true)
+    @diary_dates = DiaryDate.joins(:diary_book).where(status_admin: true,status: true, diary_books: {status_admin: true,status: true})
   end
 
   def filter_by_date
@@ -12,25 +13,15 @@ class Public::CalendarDiariesController < ApplicationController
     @date_date = date_arr[2]
     #binding.pry
     date =  Date.new(date_arr[0].to_i,date_arr[1].to_i, date_arr[2].to_i)
-    #byebug
-    @diary_dates = DiaryDate.select do |d|
-     d.start_time.to_date == date && d.status == true && d.status_admin == true && d.diary_book.status == true && d.diary_book.status_admin == true
-   end
 
-    if params[:search] == ''
-    @diary_dates.sort
-    elsif params[:search] == 'newpost'
-      #@diary_dates.sort { |a, b| b[:created_at].to_time.to_i <=> a[:created_at].to_time.to_i }
-      @diary_dates.sort do |a, b|
-        b[:created_at].in_time_zone.to_i <=> a[:created_at].in_time_zone.to_i
-     #binding.pry
-      end
-    elsif params[:search] == 'oldpost'
-      #@diary_dates.sort { |a, b| a[:created_at].to_time.to_i <=> b[:created_at].to_time.to_i }
-      @diary_dates.sort do |a, b|
-        a[:created_at].in_time_zone.to_i <=> b[:created_at].in_time_zone.to_i
-      end
-    end
+
+  if params[:search] == 'oldpost'
+    @diary_dates = DiaryDate.joins(:diary_book).where(start_time: date.at_beginning_of_day...date.at_end_of_day, status: true, status_admin: true, diary_books: {status_admin: true,status: true}).order("created_at ASC")
+  else
+    @diary_dates = DiaryDate.joins(:diary_book).where(start_time: date.at_beginning_of_day...date.at_end_of_day, status: true, status_admin: true, diary_books: {status_admin: true,status: true}).order("created_at DESC")
+  end
+
+
 
   #arr = []
   #Diary.each do |d|
