2a7405722fdace83cb42923c3a5ba38ac31c36f9
valleymouth1216 <okeitan3280@gmail.com>
2022-10-30 08:15:30 +0000

    20221030seedファイル書き方変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,68 +7,101 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Tag.create([
-    { name: '野菜' },
-    { name: '果物' },
-    { name: '花'},
-    { name: '木'},
-    { name: 'その他'}
-    ])
+tags = [
+         { name: '野菜' },
+         { name: '果物' },
+         { name: '花'},
+         { name: '木'},
+         { name: 'その他'}
+       ]
 
-Admin.create!(
-  email: "everyone_plant_diary_admin@a",
-  password: "123456"
-)
+tags.each do |tag|
+  Tag.find_or_create_by(tag)
+end
 
-customers = Customer.create!(
-  [
-    {email: 'aa@a', name: '令和', password: '123456'},
-    {email: 'hanako@test.com', name: '花子', password: '123456'},
-    {email: 'tarou@test.com', name: '太郎', password: '123456'}
-  ]
-)
+
+
+Admin.find_or_create_by!(email: "everyone_plant_diary_admin@a") do |admin|
+  admin.email = "everyone_plant_diary_admin@a"
+  admin.password = "123456"
+end
+
+
+
+customers = [
+             {email: 'aa@a', name: '令和', password: '123456'},
+             {email: 'hanako@test.com', name: '花子', password: '123456'},
+             {email: 'tarou@test.com', name: '太郎', password: '123456'}
+            ]
+
+customers.each do |customer|
+  customer_data = Customer.find_by(email: customer[:email])
+  if customer_data.nil?
+    Customer.create(
+      email: customer[:email],
+      password: customer[:password],
+      name: customer[:name],
+    )
+  end
+end
+
+
+customer_ids = Customer.ids
+tag_ids = Tag.ids
+
 
 diary_books = DiaryBook.create!(
   [
-   {customer_id: '1', title: '米',status: "true", tag_ids: [1]},
-   {customer_id: '1', title: '枝豆',tag_ids: [1]},
-   {customer_id: '1', title: '桜',tag_ids: [3]},
-   {customer_id: '2', title: '桜' ,status: "true",tag_ids: [3]},
-   {customer_id: '2', title: 'ひまわり' ,tag_ids: [3]}
+   {customer_id: customer_ids.sample, title: '米',status: "true", tag_ids: [tag_ids.sample]},
+   {customer_id: customer_ids.sample, title: '枝豆',tag_ids: [tag_ids.sample]},
+   {customer_id: customer_ids.sample, title: '桜',tag_ids: [tag_ids.sample]},
+   {customer_id: customer_ids.sample, title: '桜' ,status: "true",tag_ids: [tag_ids.sample]},
+   {customer_id: customer_ids.sample, title: 'ひまわり' ,tag_ids: [tag_ids.sample]}
   ]
 )
 
-diarydates = DiaryDate.create!(
+first_diary_book_id = DiaryBook.first.id
+second_diary_book_id =DiaryBook.limit(1).offset(1).first.id
+last_diary_book_id =DiaryBook.last.id
+
+ DiaryDate.create!(
   [
-   {diary_book_id: '1', start_time: '2022-9-13 15:00:00',temperature: '20',body: "稲を刈りしました。猪に田んぼを荒らされた(猪の大運動会のおかげで)ので手刈りました。途中コンバインも使っていましたが、壊れました。今週は筋肉痛確定です.....",status: "true"},
-   {diary_book_id: '1', start_time: '2022-9-14 15:00:00',temperature: '20',body: "稲刈り二日目です。お昼ご飯は外でみんなで食べました。いつもとは違う新鮮な感じで美味しかったです。",status: "true"},
-   {diary_book_id: '1', start_time: '2022-5-15 15:00:00',temperature: '19',body: "田植えをしました。去年よりお米がとれますように",status: "true"},
-   {diary_book_id: '1', start_time: '2022-9-16 15:00:00',body: "昨日、稲刈りしたから全身筋肉痛．．．足動かせない早く治りますように",status: "true"},
-   {diary_book_id: '1', start_time: '2022-9-20 15:00:00',body: "やったと筋肉痛から解放されました。健康でいられるって幸せだと感じるぐらいひどかったです。",status: "true"},
-   {diary_book_id: '1', start_time: '2022-9-15 15:00:00',temperature: '19',body: "お米をはぜぼししました。もう来年はやりたくないです。"},
-   {diary_book_id: '2', start_time: '2022-5-15 15:00:00',temperature: '15',body: "田舎で今日と植えようとしたら苗の葉っぱかじられて茎しかない！！どういうこと？？？",status: "true"},
-   {diary_book_id: '2', start_time: '2022-5-21 15:00:00',temperature: '15',body: "今回は苗を家の中においたからかじられずに済んだ誰に食べれているの？？",status: "true"},
-   {diary_book_id: '2', start_time: '2022-10-01 15:00:00',temperature: '15',body: "またかじれてる．．．柵でなんとかならないかな",status: "true"},
-   {diary_book_id: '2', start_time: '2022-10-08 15:00:00',temperature: '15',body: "あんなことがあったけどなんとか収穫出来ました。",status: "true"},
-   {diary_book_id: '4', start_time: '2022-04-08 15:00:00',temperature: '15',body: "桜を見に来ました。",status: "true"},
-   {diary_book_id: '4', start_time: '2022-04-09 15:00:00',temperature: '15',body: "今日も桜を見に来ました。"},
-   {diary_book_id: '2', start_time: '2022-10-09 15:00:00',temperature: '15',body: "またかじれてるどんだけかじれば気が済むの"}
+   {diary_book_id: first_diary_book_id, start_time: '2022-9-13 15:00:00',temperature: '20',body: "稲を刈りしました。猪に田んぼを荒らされた(猪の大運動会のおかげで)ので手刈りました。途中コンバインも使っていましたが、壊れました。今週は筋肉痛確定です.....",status: "true" },
+   {diary_book_id: first_diary_book_id, start_time: '2022-9-14 15:00:00',temperature: '20',body: "稲刈り二日目です。お昼ご飯は外でみんなで食べました。いつもとは違う新鮮な感じで美味しかったです。",status: "true"},
+   {diary_book_id: first_diary_book_id, start_time: '2022-5-15 15:00:00',temperature: '19',body: "田植えをしました。去年よりお米がとれますように",status: "true"},
+   {diary_book_id: first_diary_book_id, start_time: '2022-9-16 15:00:00',body: "昨日、稲刈りしたから全身筋肉痛．．．足動かせない早く治りますように",status: "true"},
+   {diary_book_id: first_diary_book_id, start_time: '2022-9-20 15:00:00',body: "やったと筋肉痛から解放されました。健康でいられるって幸せだと感じるぐらいひどかったです。",status: "true"},
+   {diary_book_id: first_diary_book_id, start_time: '2022-9-15 15:00:00',temperature: '19',body: "お米をはぜぼししました。もう来年はやりたくないです。"},
+   {diary_book_id: second_diary_book_id, start_time: '2022-5-15 15:00:00',temperature: '15',body: "田舎で今日と植えようとしたら苗の葉っぱかじられて茎しかない！！どういうこと？？？",status: "true"},
+   {diary_book_id: second_diary_book_id, start_time: '2022-5-21 15:00:00',temperature: '15',body: "今回は苗を家の中においたからかじられずに済んだ誰に食べれているの？？",status: "true"},
+   {diary_book_id: second_diary_book_id, start_time: '2022-10-01 15:00:00',temperature: '15',body: "またかじれてる．．．柵でなんとかならないかな",status: "true"},
+   {diary_book_id: second_diary_book_id, start_time: '2022-10-08 15:00:00',temperature: '15',body: "あんなことがあったけどなんとか収穫出来ました。",status: "true"},
+   {diary_book_id: last_diary_book_id, start_time: '2022-04-08 15:00:00',temperature: '15',body: "桜を見に来ました。",status: "true"},
+   {diary_book_id: last_diary_book_id, start_time: '2022-04-09 15:00:00',temperature: '15',body: "今日も桜を見に来ました。"},
+   {diary_book_id: second_diary_book_id, start_time: '2022-10-09 15:00:00',temperature: '15',body: "またかじれてるどんだけかじれば気が済むの"}
   ]
 )
 
-diary_comments = DiaryComment.create!(
+DiaryDate.first.diary_images.attach(io: File.open("#{Rails.root}/db/fixtures/20220910_092503.jpg"), filename:"20220910_092503.jpg")
+DiaryDate.first.diary_images.attach(io: File.open("#{Rails.root}/db/fixtures/20220910_092503.jpg"), filename:"20220910_092503.jpg")
+
+first_customer_id = Customer.first.id
+second_customer_id = Customer.limit(1).offset(1).first.id
+last_customer_id = Customer.last.id
+
+DiaryComment.create!(
   [
-   {customer_id: '2', diary_date_id: '2',comment: "お疲れ様です。"},
-   {customer_id: '3', diary_date_id: '2',comment: "お疲れ様です。大変でしたね。"},
-   {customer_id: '3', diary_date_id: '2',comment: "今日ですべて終わったのですか？"},
-   {customer_id: '1', diary_date_id: '2',comment: "稲刈りは終わりました。"},
-   {customer_id: '2', diary_date_id: '5',comment: "本当にお疲れ様です。"}
+   {customer_id: second_customer_id, diary_date_id: second_diary_book_id,comment: "お疲れ様です。"},
+   {customer_id: last_customer_id, diary_date_id: second_diary_book_id,comment: "お疲れ様です。大変でしたね。"},
+   {customer_id: last_customer_id, diary_date_id: second_diary_book_id,comment: "今日ですべて終わったのですか？"},
+   {customer_id: first_customer_id, diary_date_id: second_diary_book_id,comment: "稲刈りは終わりました。"},
+   {customer_id: second_customer_id, diary_date_id: '5',comment: "本当にお疲れ様です。"}
   ]
 )
-favorites = Favorite.create!(
+Favorite.create!(
   [
-   {customer_id: '2', diary_date_id: '1'},
-   {customer_id: '2', diary_date_id: '2'},
-   {customer_id: '3', diary_date_id: '1'}
+   {customer_id: last_customer_id, diary_date_id: first_diary_book_id},
+   {customer_id: second_customer_id, diary_date_id: second_diary_book_id},
+   {customer_id: last_customer_id, diary_date_id: first_diary_book_id}
   ]
 )
