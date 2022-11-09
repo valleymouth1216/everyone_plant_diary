7b513062debb3961c05ab7f87e401ea733dbe451
valleymouth1216 <okeitan3280@gmail.com>
2022-11-09 13:42:59 +0000

    20221109ゲストログインデータリセット7

--- config/schedule.rb
+++ config/schedule.rb
@@ -34,7 +34,7 @@ set :output, "#{Rails.root}/log/cron.log"
 
 every 2.minute do
   begin
-    runner "Batch::DataReset.data_reset", :environment => :development
+    runner "Batch::DataReset.data_reset"
   rescue => e
     Rails.logger.error("aborted rails runner")
     raise e
