f7eaafaa3a8cee9daaadd52bbbd039bf1493acd7
valleymouth1216 <okeitan3280@gmail.com>
2022-11-09 13:35:30 +0000

    20221109ゲストログインデータリセット6

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,12 +18,21 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
-# ログの出力先を設定
-set :output, 'var/log/cron.log'
-# production 環境で cron 実行
-set :environment, :production
-ENV.each { |k, v| env(k, v) } #追加
-every 2400.minute do
+
+
+# Rails.rootを使用するために必要
+require File.expand_path(File.dirname(__FILE__) + '/environment')
+
+# cronを実行する環境変数
+rails_env = ENV['RAILS_ENV'] || :development
+
+# cronを実行する環境変数をセット
+set :environment, rails_env
+
+# cronのログの吐き出し場所
+set :output, "#{Rails.root}/log/cron.log"
+
+every 2.minute do
   begin
     runner "Batch::DataReset.data_reset", :environment => :development
   rescue => e
