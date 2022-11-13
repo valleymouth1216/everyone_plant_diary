6d34baa27d68b50859039c6fa2836344b75ae9c8
valleymouth1216 <okeitan3280@gmail.com>
2022-11-13 19:14:03 +0900

    20221113-1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require jquery　←追加
+//= require jquery    　←追加
 //= require jquery_ujs　←追加
 
 import Rails from "@rails/ujs"
