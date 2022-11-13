f12ff4b435c2c9287376eaff72bc6243c6c73a83
valleymouth1216 <okeitan3280@gmail.com>
2022-11-13 17:59:20 +0900

    2022113複数プレビュー

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require jquery　←追加
+//= require jquery_ujs　←追加
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -14,6 +16,10 @@ import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
 
+
+// import "sample.js"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
